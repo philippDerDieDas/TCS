a9f5c7e83e29d328817c318069620f29fed13aff
philippderdiedas <philippharms1qweb.de>
2016-06-06 00:22:17 +0200

    add Backgroud; add Termine;

---
+++ app/src/main/java/de/tc_schoenningstedt/tcs/Termine.java
@@ -0,0 +1,11 @@
+package de.tc_schoenningstedt.tcs;
+
+/**
+ * Created by harmph on 05.06.2016.
+ */
+public class Termine {
+
+    public String ereignis;
+    public String datum;
+
+}
--- app/src/main/java/de/tc_schoenningstedt/tcs/TermineActivity.java
+++ app/src/main/java/de/tc_schoenningstedt/tcs/TermineActivity.java
@@ -1,27 +1,53 @@
 package de.tc_schoenningstedt.tcs;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class TermineActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView day, month, year;
 
-    CalendarView cl;
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+    private RecyclerView mRVFishPrice;
+    private AdapterTermine mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +86,138 @@ public class TermineActivity extends AppCompatActivity
 
 
 
+
         day = (TextView) findViewById(R.id.day);
         day.setText(weekDay);
 
         month = (TextView) findViewById(R.id.month);
         month.setText(month_name);
 
-        cl = (CalendarView) findViewById(R.id.cl);
-
-        
 
 
+
+
+        new AsyncFetch().execute();
+    }
+
+    private class AsyncFetch extends AsyncTask<String, String, String> {
+        ProgressDialog pdLoading = new ProgressDialog(TermineActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLade...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                // Enter URL address where your json file resides
+                // Even you can make call to php file which returns json data
+                url = new URL("https://harmph.com/test.php");
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("GET");
+
+                // setDoOutput to true as we recieve data from json file
+                conn.setDoOutput(true);
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        line = line.replaceAll("ae", "ä");
+                        line = line.replaceAll("oe", "ö");
+                        line = line.replaceAll("ue", "ü");
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+
+                    return (result.toString());
+
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+            List<Termine> data=new ArrayList<>();
+
+            pdLoading.dismiss();
+            try {
+
+                JSONArray jArray = new JSONArray(result);
+
+                // Extract data from json and store into ArrayList as class objects
+                for(int i=0;i<jArray.length();i++){
+                    JSONObject json_data = jArray.getJSONObject(i);
+                    Termine fishData = new Termine();
+                    fishData.ereignis= json_data.getString("ereignis");
+                    fishData.datum= json_data.getString("datum");
+                    data.add(fishData);
+                }
+
+                // Setup and Handover data to recyclerview
+                mRVFishPrice = (RecyclerView)findViewById(R.id.rvterm);
+                mAdapter = new AdapterTermine(TermineActivity.this, data);
+                mRVFishPrice.setAdapter(mAdapter);
+                mRVFishPrice.setLayoutManager(new LinearLayoutManager(TermineActivity.this));
+
+            } catch (JSONException e) {
+                Toast.makeText(TermineActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }
 
     }
 
